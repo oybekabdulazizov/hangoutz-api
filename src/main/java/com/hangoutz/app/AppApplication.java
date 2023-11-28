a5e9e87736e581fc8069271669cd7d843bb5512b
Oybek Abdulazizov <oybek.tulqinovich@gmail.com>
2023-11-28 12:11:44 +0100

    initial commit

---
+++ src/main/java/com/hangoutz/app/AppApplication.java
@@ -0,0 +1,22 @@
+package com.hangoutz.app;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner cmdRunner() {
+		return runner -> {
+			System.out.println("Hello from the CommandLineRunner!");
+		};
+	}
+
+}
