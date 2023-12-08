dc2eca8dc7fffdfc0af08106801350a8c9126b28
Oybek Abdulazizov <oybek.tulqinovich@gmail.com>
2023-12-08 19:09:31 +0100

    add spring security user (user@password) and protect POST, PUT, DELETE endpoints with it

---
+++ src/main/java/com/hangoutz/app/auth/SecurityConfig.java
@@ -0,0 +1,47 @@
+package com.hangoutz.app.auth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+        UserDetails user =
+                User.builder().username("user").password("{noop}password").roles("USER").build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests(
+                configurer ->
+                        configurer
+                                .requestMatchers(HttpMethod.GET, "**").permitAll()
+                                .requestMatchers(HttpMethod.POST, "/api/auth/login").permitAll()
+                                .requestMatchers(HttpMethod.POST, "/api/events").hasRole("USER")
+                                .requestMatchers(HttpMethod.PUT, "/api/events/**").hasRole("USER")
+                                .requestMatchers(HttpMethod.DELETE, "/api/events/**").hasRole("USER")
+                                .requestMatchers(HttpMethod.POST, "/api/users").hasRole("USER")
+                                .requestMatchers(HttpMethod.PUT, "/api/users/**").hasRole("USER")
+                                .requestMatchers(HttpMethod.DELETE, "/api/users/**").hasRole("USER")
+        );
+
+        httpSecurity.httpBasic(Customizer.withDefaults());
+        httpSecurity.csrf(csrf -> csrf.disable());
+
+        return httpSecurity.build();
+    }
+}
