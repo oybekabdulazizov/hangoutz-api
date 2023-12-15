cb135705034b35dab889f537d8d61e00b55b5216
Oybek Abdulazizov <oybek.tulqinovich@gmail.com>
2023-12-15 22:17:58 +0100

    add category entity

---
+++ src/main/java/com/hangoutz/app/model/Category.java
@@ -0,0 +1,30 @@
+package com.hangoutz.app.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "category")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id")
+    private String id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
