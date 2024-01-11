65a1d59a621de46ad2313b8c19339236b87bf9e8
Oybek Abdulazizov <oybek.tulqinovich@gmail.com>
2024-01-11 20:16:16 +0100

    configure cross-origin for category controllers

--- src/main/java/com/hangoutz/app/controller/CategoryController.java
+++ src/main/java/com/hangoutz/app/controller/CategoryController.java
@@ -13,6 +13,7 @@ import java.util.List;
 import java.util.Map;
 
 @RequiredArgsConstructor
+@CrossOrigin(originPatterns = "*", allowCredentials = "true", allowedHeaders = "*")
 @RestController
 @RequestMapping("/api/v1")
 public class CategoryController {
